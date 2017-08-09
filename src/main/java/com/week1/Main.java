76a846e50a5b37a47073cbcadbc54f8fbcd5d0f1
clara <clara@clara.com>
2017-08-09 15:21:33 -0500

    Hello input - IntelliJ project set up to use input-utils library

---
+++ src/main/java/com/week1/Main.java
@@ -0,0 +1,19 @@
+package com.week1;
+
+import static input.InputUtils.*;
+
+/**
+ * Input and output.
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        /* Write your code here */
+        
+        String name = stringInput("Please enter your name?");
+        System.out.println("Hello, " + name + "!");
+        
+    }
+    
+}
